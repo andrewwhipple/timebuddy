45b1a7a5e8059f7d418ffdec38577ef9b31717ac
vagrant <vagrant@precise32.(none)>
2014-02-06 21:47:19 +0000

    Added javascript to edit goals

---
+++ public/js/goals-page.js
@@ -0,0 +1,54 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+$(".button").click(submitClicked);	
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	console.log("Javascript!");
+}
+
+
+
+
+function submitClicked(e) {
+	console.log("CLICKED");
+	e.preventDefault();
+	var name = $("#name").val();
+	var target = $("#target").val();
+	if ((name != "") && (target != "")) {
+		$(".goalList").append('<div class="goal"><h3>' + name + ": " + target + '</h3> <button type="button" class="editGoalButton">Edit Goal</button> <button type="button" class="deleteGoalButton">Delete Goal</button> </div>');
+	
+		$(".editGoalButton").click(editGoalClicked);
+		$(".deleteGoalButton").click(deleteGoalClicked);
+	
+	}
+}
+
+function deleteGoalClicked(e) {
+	e.preventDefault();
+	$(this).closest(".goal").remove();
+		
+}
+
+function editGoalClicked(e) {
+	e.preventDefault();
+	console.log("CLICKED SON");
+	//append a form that asks for name and Target (hrs/week)
+	//add a submit button listener
+	//when submit button clicked, pluck out the number
+	//delete the form
+	var target = "0";
+	var name = "Sleep";
+	$(this).closest(".goal").html('<div class="goal"><h3>' + name + ": " + target + '</h3> <button type="button" class="editGoalButton">Edit Goal</button> <button type="button" class="deleteGoalButton">Delete Goal</button> </div>');
+	//replace the html w/ the same, but a new "target"
+
+	$(".editGoalButton").click(editGoalClicked);
+	$(".deleteGoalButton").click(deleteGoalClicked);	
+	
+}
