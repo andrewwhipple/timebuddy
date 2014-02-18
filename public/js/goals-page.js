dde4c5976ed5ef7aaace7165edcbcd73fcfd1c21
vagrant <vagrant@precise32.(none)>
2014-02-18 22:12:48 +0000

    Added error checking to the edit goals page (aka bastardizing the awesome code Lucy wrote for the front page....)

--- public/js/goals-page.js
+++ public/js/goals-page.js
@@ -23,12 +23,26 @@ function submitClicked(e) {
 	e.preventDefault();
 	var name = $("#name").val();
 	var target = $("#target").val();
+	var hasTime = checkInput(target);
+	if (name == "") {
+		$('#message').removeClass();
+		$('#message').addClass("alert alert-warning");
+		$('#message').html("Please enter the name of an activity.");
+		return;	
+	} else if(!hasTime){
+		$('#message').removeClass();
+		$('#message').addClass("alert alert-warning");
+		$('#message').html("Please enter the amount of time you plan to spend on this activity.");
+		console.log("Problem yo");
+		return;
+	}
 	if ((name != "") && (target != "")) {
 		//$(".goalList").append('<div class="goal"><h4>' + name + ": " + target + '</h4> <button type="button" class="editGoalButton"><a href="/editindividual">Edit Goal</a></button> </div>');
 		var parameters = {'activity': name, 'hours': 0, 'goal': target};
 		$.get('/addactivitydata', parameters, writeData);
 		//$(".editGoalButton").click(editGoalClicked);
 		//$(".deleteGoalButton").click(deleteGoalClicked);
+	
 	
 	}
 	
@@ -41,9 +55,21 @@ function submitClicked(e) {
 	data["activities"].push(newActivity);*/
 }
 
+function checkInput(time){
+	if(time == "")
+		return false;
+	var regexp = /^[0-9]*(.)[0-9]*$/;
+    return (time.search(regexp) >= 0) ? true : false;
+}
+
 function writeData(results) {
 	console.log("STUFF WORKS YAY");
 	//$(".goalList").append('<div class="goal"><h4>' + results.activity + ": " + results.goal + ' hrs/week</h4> <button type="button" class="editGoalButton"><a href="/editindividual">Edit Goal</a></button> </div>');
+	
+	$('#message').removeClass();
+	$('#message').addClass("alert alert-success");
+	$('#message').html('Activity added successfully!');
+	
 	$(".goalList").append('<div class="well well-sm"> <div class = "well-text">' + results.activity + ': ' + results.goal + ' hrs/week</div></div>');
 	console.log($(".goalList").length);
 }
