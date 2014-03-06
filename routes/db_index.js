a70db336df7d531d17a786abd9975dc9b9db512d
Andrew Whipple <vagrant@precise32.(none)>
2014-03-06 01:44:23 +0000

    Trying a thing...

--- routes/db_index.js
+++ routes/db_index.js
@@ -16,7 +16,7 @@ exports.view = function(req, res){
 	
 	function renderActivities(err, activities){
 		console.log("herro rucy");
-		res.render('index', {'activities': activities});
+		res.render('index', {'activities': activities}, username);
 		console.log(activities);
 	}
 }
