d89b71f8be5f3a12664de6c5ca1f8c3e1a460b39
Whitney Wells <whitneyw@stanford.edu>
2014-03-14 14:39:13 -0700

    WE R WINE BOTTLES

--- routes/db_addactivitydata.js
+++ routes/db_addactivitydata.js
@@ -23,6 +23,16 @@ exports.addActivity = function(req, res) {
 	}
 	result = {"activity": req.query.activity, "goal": req.query.goal};
 
+	$("#targetsdiv").append("<div class='row activity well well-sm' id='"+name+"''>"
+		+"<div class='well-text inline activityname'> "+name+"</div>"
+		+"<div  class='inline' id='timespent'><em> 0 / "+target+"hrs </em></div><div class='progress'>"
+		+"<div id='progress_bar' class='progress-bar' role='progressbar'>"+
+		"</div></div><div class='inline addhours' ><form class ='inline' role='form'> "
+		+"<div class='inline time form-group float-right'><span class='glyphicon glyphicon-time' ></span>"
+		+"<a href='#'><span class='glyphicon glyphicon-minus submitBtn' id ='minusBtn'></span></a>"
+		+"<input type='text' id='time' maxlength='4' size='4' placeholder='(hrs)'></input>"
+		+"<a href='#'><span class='glyphicon glyphicon-plus submitBtn' id='plusBtn'></span></a>"
+		+"<input type='hidden' id='hoursspent' value='0'/></div> </form></div></div>");
 	res.json(result);
-
+	console.log("going back");
 }
